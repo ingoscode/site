3483b380b5cc182f765e02d62a576b48f9e0fd29
Romon001 <roman.ruchin@mail.ru>
2019-07-25 00:15:47 +0300

    Накидал и оживил картинки с галереей,причесал пост с новостями под стиль главной страницы

---
+++ Page_departments/modal_windows.js
@@ -0,0 +1,29 @@
+// JavaScript source code
+{
+    let modal = document.querySelector('.modal');
+    let img = document.querySelectorAll('.open-modal');
+    let modalImg = document.getElementById('modal-image');
+    let close = document.querySelector('.close-button');
+
+    console.log(img.length);
+    console.log(typeof modal, modal);
+
+    for (let i = 0; i < img.length; i++) {
+        img[i].addEventListener('click', e => {
+            console.log('fire click');
+            modal.style.display = 'block';
+            modalImg.src = e.target.src;
+        })
+    }
+
+    modal.addEventListener('click', e => {
+            let element = e.target.className;
+            if (element == 'modal')
+                modal.style.display = 'none';
+        }
+    );
+    close.addEventListener('click', e => {
+            modal.style.display = 'none';
+        }
+    );
+}
